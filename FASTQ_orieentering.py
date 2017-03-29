cd58721361faa3dda63babe8f5a43e88d13ed3eb
J35P312 <jesper.eisfeldt@scilifelab.se>
2017-03-29 09:16:29 +0200

    new file:   FASTQ_orieentering.py 	new file:   README.md

---
+++ FASTQ_orieentering.py
@@ -0,0 +1,21 @@
+import sys
+import gzip
+
+line = 0
+orientation=1
+
+
+read_pair=[]
+for line in gzip.open(sys.argv[1]):
+	read_pair.append( line.strip() )
+	if len(read_pair) == 8:
+		if read_pair[0] == read_pair[4]:
+			read_pair[0] += " 1:::"
+			read_pair[4] += " 2:::"
+		else:
+			print "ERROR: unpaired read/disordered fastq"
+			quit()
+
+		for read in read_pair:
+			print read
+		read_pair = []
---
+++ README.md
@@ -0,0 +1,3 @@
+# fastq_orienteering
+
+python FASTQ_orienteering.py input.fastq.gz > output.fastq
